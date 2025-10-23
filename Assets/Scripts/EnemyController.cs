f681379d7fd7d334407acedb4fc3d943a7f982fc
sthasafal <shresthasafal72@gmail.com>
2025-10-22 20:22:47 -0700

    Added Player Attack System

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyController  : MonoBehaviour
 {
     public float speed = 2f;
     [Header("Patrol range (relative to start X)")]
